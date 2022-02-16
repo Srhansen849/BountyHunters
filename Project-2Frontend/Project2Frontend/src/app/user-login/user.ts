1b3ecb8cc155c774c69f2440c40fd6ec6c3c3cde
siemtime <96088482+siemtime@users.noreply.github.com>
2022-02-16 16:21:47 -0600

    update2-16

--- Project-2Frontend/Project2Frontend/src/app/user-login/user.ts
+++ Project-2Frontend/Project2Frontend/src/app/user-login/user.ts
@@ -1,10 +1,9 @@
 export class User{
-    //username:string;
-    //password:password;
-    constructor(public username:string, public password:string, public userid?: number, public firstname?: string, public lastname?: string,
-        public email?: string, public rank?: number){
-        //this.username=username;
-        //this.password=password;
-
+       
+    constructor(public username:string, public password:string, 
+                public userid?: number, public firstname?: string, 
+                public lastname?: string, public email?: string, 
+                public rank?: number, public codename?: string
+                ){
     }
 }
