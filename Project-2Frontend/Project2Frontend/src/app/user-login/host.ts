845544a5bfd520f3b579c8b0fd421ed008436e57
Sengthay Kaing <skthay2@gmail.com>
2022-02-16 15:51:48 -0700

    update ts files

---
+++ Project-2Frontend/Project2Frontend/src/app/user-login/host.ts
@@ -0,0 +1,8 @@
+export class Host {
+    constructor(public username: string, public password: string,
+        public hostid?: number, public firstname?: string,
+        public lastname?: string, public email?: string,
+        public association?: string, public representative?: string,
+        public codename?: string) { }
+
+}
--- Project-2Frontend/Project2Frontend/src/app/user-login/user.ts
+++ Project-2Frontend/Project2Frontend/src/app/user-login/user.ts
@@ -1,13 +1,12 @@
 export class User {
-    //username:string;
-    //password:password;
-    constructor(public username: string, public password: string, public userid?: number, public firstname?: string, public lastname?: string,
-        public email?: string, public rank?: number) {
-        //this.username=username;
-        //this.password=password;
-
+    constructor(public username: string, public password: string,
+        public userid?: number, public firstname?: string,
+        public lastname?: string, public email?: string,
+        public rank?: number, public codename?: string
+    ) {
     }
 }
+
 
 /*
 @Id
