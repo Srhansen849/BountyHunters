44fd41f7ffca34ead6cb4bc844f6e743b0c00d72
Sengthay Kaing <skthay2@gmail.com>
2022-02-17 21:28:24 -0700

    files

--- Project-2Frontend/Project2Frontend/src/app/app.component.ts
+++ Project-2Frontend/Project2Frontend/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { UserLoginComponent } from './user-login/user-login.component';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Project2Frontend';
+
+  onFile(event) {
+    console.log(event);
+
+  }
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+
 }
