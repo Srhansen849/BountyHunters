8c226c01f730276984f260f3d45c04e71950bb7c
Sengthay Kaing <skthay2@gmail.com>
2022-02-21 19:34:34 -0700

    starting cucumber test

--- Project-2/src/test/java/com/example/gluecode/bountiesTableTest.java
+++ Project-2/src/test/java/com/example/gluecode/bountiesTableTest.java
@@ -1,7 +1,70 @@
 package com.example.gluecode;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class bountiesTableTest {
 	
-	
+
+@Given("the Business Owner is on the login page")
+public void the_business_owner_is_on_the_login_page() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("the Business Owner selects the Business Owner Login")
+public void the_business_owner_selects_the_business_owner_login() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+@When("the Business Owner inputs {string} into the username field")
+public void the_business_owner_inputs_into_the_username_field(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+@When("the Business Owner inputs {string} into the password field")
+public void the_business_owner_inputs_into_the_password_field(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+@When("the Business Owner clicks login")
+public void the_business_owner_clicks_login() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+@Then("the Business Owner is directed to the home page")
+public void the_business_owner_is_directed_to_the_home_page() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+@When("the Business Owner clicks the Confirm a Bounty link")
+public void the_business_owner_clicks_the_confirm_a_bounty_link() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+@When("the Business Owner selects a completed bounty")
+public void the_business_owner_selects_a_completed_bounty() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+@When("the Business Owner add an {string}")
+public void the_business_owner_add_an(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+@When("the Business Owner submits the information")
+public void the_business_owner_submits_the_information() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+@Then("the bounty will be registered as complete")
+public void the_bounty_will_be_registered_as_complete() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+
+
 
 }
