9bbfb5da8b90186fa5d98dfe12b45c074b99ff9e
Sengthay Kaing <skthay2@gmail.com>
2022-02-23 08:56:39 -0700

    change

--- Project-2Frontend/Project2Frontend/src/app/bounty/objects/host-object.ts
+++ Project-2Frontend/Project2Frontend/src/app/bounty/objects/host-object.ts
@@ -1,10 +1,18 @@
 import { Bounty } from "./bounty-object";
 
 export class Host {
+<<<<<<< HEAD
     constructor(public username: string, public password: string,
         public association?: string, public codename?: string,
         public email?: string, public firstname?: string,
         public lastname?: string,
         public representative?: string,
         public bounty_list?: Bounty[], public hostid?: number) { }
+=======
+    constructor(public association: string, public codename: string,
+        public email: string, public firstname: string,
+        public lastname: string, public password: string,
+        public representative: string, public username: string,
+        public bounty_list: Bounty[], public hostid?: number) { }
+>>>>>>> start the css
 }
